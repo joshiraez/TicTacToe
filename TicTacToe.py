dcfb293fab6e336b904d858c8b773eedd961a008
Jose 'Joshi' Raez <joseraezrodriguez@gmail.com>
2017-11-29 01:04:35 +0100

    Check result and main program (just game()) finished.

--- TicTacToe.py
+++ TicTacToe.py
@@ -5,11 +5,12 @@ def printBoard(currentBoard):
     boardTiles = ('  1 2 3', ' ┌-┬-┬-┐', ' ├-┼-┼-┤', ' └-┴-┴-┘')
 
     print(boardTiles[0])
+    print(boardTiles[1])
     for row in range(2):
         printRow(currentBoard, row)
-        print(boardTiles[1])
+        print(boardTiles[2])
     printRow(currentBoard, 2)
-    print(boardTiles[2])
+    print(boardTiles[3])
 
 def printRow(currentBoard, row):
     boardRows = (' ', 'O', 'X', '│')
@@ -24,7 +25,7 @@ def game():
     printBoard(board)
     result = None
     player = 1
-    while result: #None will evaluate to False. String will evaluate to True.
+    while result is None:
         putPiece(board, player)
         printBoard(board)
         result = checkResult(board)
@@ -35,9 +36,9 @@ def game():
 
 
 def putPiece(board, player): #Needs exception handling
-    ok = True
+    ok = False
     while not ok:
-        print('Player {} please introduce row and column for your move').format(player)
+        print('Player {} please introduce row and column for your move'.format(player))
 
         row =  int(input('Row (1-3):'))-1
         while not 0<=row<=2:
@@ -48,9 +49,55 @@ def putPiece(board, player): #Needs exception handling
             column = int(input('Wrong number. Column (1-3):')) -1
 
         if board[row][column]==0:
-            board[row][column]==player
-
+            board[row][column]=player
+            ok = True
         else:
             print("You can't put a piece there. Select another position.")
             ok = False
 
+def checkResult(board):
+    from collections import Counter
+    openPieces=0
+    possibleResults = ('Draw', 'Player 1 wins', 'Player 2 wins')
+    result=None
+
+    #rows
+    # we could use break, but I think it's generally better to avoid sequence breaking
+    for row in range(3):
+        count = Counter(board[row])
+        if count[0] > 0:
+            openPieces += count[0]
+        elif count[1] == 3:
+            result = possibleResults[1]
+        elif count[2] == 3:
+            result = possibleResults[2]
+
+    if result is None:
+        #columns
+        for column in range(3):
+            count = Counter([row[column] for row in board])
+            if count[1] == 3:
+                result = possibleResults[1]
+            elif count[2] == 3:
+                result = possibleResults[2]
+
+        #diagonals:
+        count = Counter([board[i][i] for i in range(3)])
+        if count[1] == 3:
+            result = possibleResults[1]
+        elif count[2] == 3:
+            result = possibleResults[2]
+
+        count = Counter([board[2-i][i] for i in range(3)])
+        if count[1] == 3:
+            result = possibleResults[1]
+        elif count[2] == 3:
+            result = possibleResults[2]
+
+    if result is None and openPieces == 0:
+        result = possibleResults[0]
+
+    return result
+
+#mainProgram
+game()
